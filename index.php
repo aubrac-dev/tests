c1c9a0558412992c3843e6dbb1720c3da1e215bd
Ioan Ovidiu Toma <87045064+aubrac-dev@users.noreply.github.com>
2021-10-11 16:55:13 +0200

    transm param

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>Envoi de paramètres dans l'URL</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+
+<body>
+
+    <p><a href="bonjour.php?nom=Dupont&prenom=Jean">Dis-moi bonjour !</a></p>
+
+</body>
+
+</html>
