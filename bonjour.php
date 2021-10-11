91a4fc13d9b13025ce748812f7157d28fc20a2c0
Ioan Ovidiu Toma <87045064+aubrac-dev@users.noreply.github.com>
2021-10-11 16:53:54 +0200

    transmettez des donnes

---
+++ bonjour.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>Réception de paramètres dans l'URL</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+
+<body>
+
+    <p>Bonjour <?php echo $_GET['prenom'] . ' ' . $_GET['nom']; ?> !</p>
+
+</body>
+
+</html>
--- info.php
+++ info.php
@@ -1,3 +1,29 @@
 <?php
 
-// Transmettez des données avec l'URL
+// 1. Transmettez des données avec l'URL
+
+// http://www.monsite.com/bonjour.php?nom=Dupont&prenom=Jean
+
+// <a href="bonjour.php?nom=Dupont&amp;prenom=Jean">Dis-moi bonjour !</a>
+
+// 2. Récupérer les paramètres en PHP
+
+// index.php -> bonjour.php
+
+// 3. Ne faites jamais confiance aux données reçues !
+
+// Tous les visiteurs peuvent trafiquer les URL
+
+// Tester la présence d'un paramètre
+
+/*
+http://localhost/tests/bonjour.php
+
+ On a essayé d'afficher la valeur de $_GET['prenom']  et celle de $_GET['nom']  ... 
+ Mais comme on vient de les supprimer de l'URL, ces variables n'ont pas été créées, 
+ et donc elles n'existent pas .
+
+ PHP nous avertit qu'on essaie d'utiliser des variables qui n'existent pas, d'où les « Undefined index »
+*/
+
+// Cette fonction teste si une variable existe: isset()
