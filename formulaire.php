975f1fafbf041da6651f1fbb8a862ec8d6adce7d
Ioan Ovidiu Toma <87045064+aubrac-dev@users.noreply.github.com>
2021-10-12 10:45:49 +0200

    passwd

--- formulaire.php
+++ formulaire.php
@@ -2,15 +2,16 @@
 <html lang="fr">
 
 <head>
-    <title>Envoi de paramètres dans le formulaire</title>
+    <title>Page protégée par mot de passe</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 </head>
 
 <body>
 
     <p>
-        Veuillez taper votre mot de pass :
+        Veuillez entrer le mot de passe pour obtenir les codes d'accès au serveur central de la NASA :
     </p>
+
     <form action="secret.php" method="post">
         <p>
             <label for="pwd">Mot de pass:</label>
@@ -18,6 +19,9 @@
             <input type="submit" value="Valider" />
         </p>
     </form>
+
+    <p>Cette page est réservée au personnel de la NASA.
+        Si vous ne travaillez pas à la NASA, inutile d'insister vous ne trouverez jamais le mot de passe ! ;-)</p>
 </body>
 
 </html>
--- secret.php
+++ secret.php
@@ -1,30 +1,29 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html>
 
 <head>
-    <title>Envoi de paramètres dans l'URL</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta charset="utf-8" />
+    <title>Codes d'accès au serveur central de la NASA</title>
 </head>
 
 <body>
 
     <?php
-    $_POST['mot_de_passe'] = (string) $_POST['mot_de_passe'];
-    if (isset($_POST['mot_de_passe'])) {
-        if ($_POST['mot_de_passe'] == 'kangourou') {
-            echo ('<p>Mot de pass ' . $_POST['mot_de_passe'] . ' OK.</p>');
-        } else {
+    if (isset($_POST['mot_de_passe']) and $_POST['mot_de_passe'] ==  "kangourou") // Si le mot de passe est bon
+    {
+        // On affiche les codes
     ?>
-            <p> Mot de passe <?php echo $_POST['mot_de_passe']; ?> incorrect:</br>
-                Si tu veux changer de saisir, <a href="formulaire.php">clique ici</a>
-                pour revenir à la page formulaire.php.
-            </p>
-        <?php
-        }
-    } else {
-        ?>
-        <p> ATTENTION <a href="formulaire.php">Veuillez entrer un mot de passe!</a>dans la page formulaire.php.</p>
+        <h1>Voici les codes d'accès :</h1>
+        <p><strong>CRD5-GTFT-CK65-JOPM-V29N-24G1-HH28-LLFV</strong></p>
+
+        <p>
+            Cette page est réservée au personnel de la NASA. N'oubliez pas de la visiter régulièrement car les codes d'accès sont changés toutes les semaines.<br />
+            La NASA vous remercie de votre visite.
+        </p>
     <?php
+    } else // Sinon, on affiche un message d'erreur
+    {
+        echo '<p>Mot de passe incorrect</p>';
     }
     ?>
 
