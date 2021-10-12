168ee2c9f4f1dc5b25bbedf614bf24e7c64ed9eb
Ioan Ovidiu Toma <87045064+aubrac-dev@users.noreply.github.com>
2021-10-12 12:10:28 +0200

    cookies

---
+++ lire_cookies.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Ma super page PHP</title>
+</head>
+
+<body>
+
+    <p>
+        Hé ! Je me souviens de toi !<br />
+        Tu t'appelles <?php echo $text = (string) isset($_COOKIE['pseudo']) ? $_COOKIE['pseudo'] : 'the cookie pseudo is not set'; ?> et tu viens de <?php echo $_COOKIE['pays']; ?> c'est bien ça ?
+    </p>
+
+</body>
+
+</html>
